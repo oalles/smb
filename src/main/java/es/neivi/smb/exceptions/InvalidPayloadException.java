08f1cd63e5513b660cc9f5d608110ee5d384026b
oalles <oalles@neivi.es>
2016-01-27 15:17:10 +0100

    MB RuntimeExceptions to be considered

---
+++ src/main/java/es/neivi/smb/exceptions/InvalidPayloadException.java
@@ -0,0 +1,24 @@
+package es.neivi.smb.exceptions;
+
+public class InvalidPayloadException extends MBException {
+
+	public InvalidPayloadException() {
+	}
+
+	public InvalidPayloadException(String message) {
+		super(message);
+	}
+
+	public InvalidPayloadException(Throwable cause) {
+		super(cause);
+	}
+
+	public InvalidPayloadException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public InvalidPayloadException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+}
