8ce5d3ad1caa705afec6655ecbe8eae19228d025
oalles <oalles@neivi.es>
2016-01-27 15:18:32 +0100

    Polishing

--- src/main/java/es/neivi/smb/annotation/EnableSMB.java
+++ src/main/java/es/neivi/smb/annotation/EnableSMB.java
@@ -8,7 +8,6 @@ import java.lang.annotation.Target;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.scheduling.annotation.Async;
 
 /**
  * Enables Simple Mongo Broadcaster capability, to be used on @
@@ -18,20 +17,8 @@ import org.springframework.scheduling.annotation.Async;
  * &#064;Configuration
  * &#064;EnableSMB(consumerId=#{env.getRequiredProperty("smb.consumer_id")})
  * public class AppConfig {
- * 	&#064;Bean
- * 	public MyAsyncBean asyncBean() {
- * 		return new MyAsyncBean();
- * 	}
- * }
  * </pre>
  *
- * where {@code MyAsyncBean} is a user-defined type with one or methods
- * annotated with @{@link Async} (or any custom annotation specified by the
- * {@link #annotation()} attribute).
- *
- * @see RootMessageEntity
- * @see SMBConfigurer
- * @see SMBConfiguration
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE })
